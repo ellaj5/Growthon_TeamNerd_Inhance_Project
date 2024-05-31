4f499451bcd82b5dfaef52a53f4ef5603a25f770
ellaj5 <misosally050122@gmail.com>
2024-05-31 13:53:17 +0900

    update final xml

--- app/src/main/java/com/nerd/inhance/realfinal/StudyBoardMathActivity.java
+++ app/src/main/java/com/nerd/inhance/realfinal/StudyBoardMathActivity.java
@@ -1,6 +1,5 @@
 package com.nerd.inhance.realfinal;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,21 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class StudyBoardMathActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_study_board_math);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Intent intent = new Intent(getApplicationContext(), StudyBoardMathActivity.class);
-        startActivity(intent);
-        finish();
     }
 }
